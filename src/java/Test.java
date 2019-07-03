5e1cce3c2f2e9fea65b1c7f9ebca4c8e1ee93a7a
miracleyanny <314547718@qq.com>
2019-07-03 10:18:16 +0800

    提交java

---
+++ src/java/Test.java
@@ -0,0 +1,9 @@
+package src.java;
+public class Test {
+    public static void main(String[] args) {
+        if (null == null) {
+            System.out.println("It is null");
+        }
+    }
+}
+
